f148f16db3e84ba65f04bfa2b6e23d998b43d1dd
alicekres <alice.kreslins@gmail.com>
2022-05-16 14:50:20 +0100

    add form with button, try fetch and display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,40 @@ export const fetchGeneral = () => {
   fetch('http://localhost:3000/general')
     .then((res) => res.json())
     .then((data) => {
+      const div = document.querySelector('.general');
+      data.forEach(() => {
+        const newTitle = document.createElement('h3');
+        const newP = document.createElement('p');
+
+        newTitle.textContent = data.name;
+        newP.textContent = data.body;
+
+        div.appendChild(newTitle);
+        div.appendChild(newP);
+      });
       console.log(data);
     });
 };
+// export const fetchAttractions = () => {
+//   fetch('http://localhost:3000/attractions')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
+
+// export const fetchAttractions = () => {
+//   fetch('http://localhost:3000/attractions')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
+
+// export const fetchPlaces = () => {
+//   fetch('http://localhost:3000/places')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,30 @@
 //Things that directly affect the DOM, event listeners etc
 import { fetchGeneral } from './app.js';
-
+// import { fetchAttractions } from './app.js;';
+console.log('hi');
 fetchGeneral();
+// fetchAttractions();
+
+// const submitButton = document.querySelector('.submitButton');
+
+// function submitGeneral(e) {
+//   e.preventDefault();
+//   const generalData = {
+//     title: e.target.title.value,
+//     body: e.target.body.value,
+//   };
+//   const options = {
+//     method: 'POST',
+//     body: JSON.stringify(generalData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   };
+//   fetch('http://localhost:3000/general', options)
+//     .then((r) => r.json())
+//     .catch(console.warn);
+// }
+
+// submitButton.addEventListener('submit', () => {
+//   submitGeneral();
+// });
