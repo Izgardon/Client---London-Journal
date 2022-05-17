0c7da747c7a6c38709b9ecf5f77dcda25376c75d
William <williamsessions23@gmail.com>
2022-05-17 14:36:38 +0100

    added complete functionality of displaying and adding posts on all 3 dataTypes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,7 @@
 /* import { postNewPost } from "./app.js"; */
 //Things that directly affect the DOM, event listeners etc
 
-const btnPost = document.querySelector(".general-btn");
-const generalTitle = document.querySelector(".general-title");
-const generalBody = document.querySelector(".general-body");
-const generalPosts = document.querySelector(".general-posts");
+const postBtns = document.querySelectorAll(".form-btn");
 const attractionsPosts = document.querySelector(".attractions-posts");
 const placesPosts = document.querySelector(".places-posts");
 
@@ -16,19 +13,21 @@ getAllPosts("places");
 
 //Event listeners
 
-btnPost.addEventListener("click", (e) => {
-  /* e.preventDefault(); */
-  console.log("hello");
-  let dataType = e.target.id;
+postBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-  const postData = {
-    title: eval(`${dataType + "Title"}`).value,
-    body: eval(`${dataType + "Body"}`).value,
-  };
-  postNewPost(dataType, postData);
+    let dataType = e.target.id;
 
-  eval(`${dataType + "Title"}`).value = "";
-  eval(`${dataType + "Body"}`).value = "";
+    const postData = {
+      title: document.querySelector(`.${dataType}-title`).value,
+      body: document.querySelector(`.${dataType}-body`).value,
+    };
+    postNewPost(dataType, postData);
+
+    document.querySelector(`.${dataType}-title`).value = "";
+    document.querySelector(`.${dataType}-body`).value = "";
+  });
 });
 
 //Functions
@@ -68,7 +67,7 @@ function postNewPost(dataType, post) {
 function append(dataType, post) {
   let page = Math.ceil(post.id / 3);
   if (post.id % 3 == 1) {
-    eval(`${dataType}Posts`).insertAdjacentHTML(
+    eval(document.querySelector(`.${dataType}-posts`)).insertAdjacentHTML(
       "beforeend",
       `<div class="carousel-item ${
         page == 1 ? "active" : ""
@@ -76,7 +75,7 @@ function append(dataType, post) {
     );
     document.querySelector(`.${dataType}-${page}`).insertAdjacentHTML(
       "beforeend",
-      ` <div class="card main-card m-3" id="${post.id} "style="width: 18rem;">
+      ` <div class="card main-card m-3" id="${dataType}-${post.id}" style="width: 18rem;">
                             
                     <div class="card-body">
                       <h5 class="card-title">${post.title}</h5>
@@ -104,7 +103,7 @@ function append(dataType, post) {
 
 //Modals
 
-const formModal = document.getElementById("form-general");
+/* const formModal = document.getElementById("form-general");
 formModal.addEventListener("show.bs.modal", (event) => {
   // Button that triggered the modal
   const button = event.relatedTarget;
@@ -117,3 +116,4 @@ formModal.addEventListener("show.bs.modal", (event) => {
   const modalTitle = formModal.querySelector(".modal-title");
   const modalBodyInput = formModal.querySelector(".modal-body input");
 });
+ */
