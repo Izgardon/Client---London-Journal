37f98434aacf16c1adba3eb1c551527c7df5b809
alicekres <alice.kreslins@gmail.com>
2022-05-17 17:42:18 +0100

    add tests

--- _tests_/api.test.js
+++ _tests_/api.test.js
@@ -6,3 +6,17 @@ const fs = require('fs');
 const path = require('path');
 
 const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
+describe('index.html', () => {
+  test('html head has a title', () => {
+    document.documentElement.innerHTML = html.toString();
+    const title = document.querySelector('title');
+    expect(title.textContent).toBe('London Travel');
+  });
+
+  test('html head has a favicon', () => {
+    document.documentElement.innerHTML = html.toString();
+    const favicon = document.querySelector('.favicon');
+    expect(favicon.rel).toBe('icon');
+  });
+});
